fb911def50cb4d14e5fb64cd392d6bff87456cba
hlb <hlbyichi@gmail.com>
2017-08-01 14:28:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# USAGE
+
+    scrapy crawl generic -a domain="example.com" -a url="http://example.com/" -a pattern=".html" -o example.json
---
+++ generic_parser/spiders/generic.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+class GenericSpider(scrapy.Spider):
+    name = 'generic'
+    allowed_domains = ['example.com']
+    start_urls = ['http://example.com/']
+
+    def __init__(self, domain='', url='', pattern='', *args, **kwargs):
+        super(GenericSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [url]
+        self.allowed_domains = [domain]
+        self.pattern = pattern
+
+    def parse(self, response):
+        self.logger.info('URL: %s', response.url)
+        self.logger.info('pattern: %s', self.pattern)
+        for href in response.css('a::attr(href)'):
+            if href is not None:
+                if re.search(self.pattern, href.extract()):
+                    yield response.follow(href, self.parse_item)
+
+    def parse_item(self, response):
+        self.logger.info('ITEM URL: %s', response.url)
+        yield {
+            'title': response.css('title::text').extract_first(),
+            'url': response.url,
+            'canonical': response.css('meta[property="canonical"]::attr(content)').extract_first(),
+            'og_url': response.css('meta[property="og:url"]::attr(content)').extract_first(),
+        }
